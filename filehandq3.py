ac7a32776a637dc436a120f510a28b04ddcf9a86
SHIVANSH666 <92948571+SHIVANSH666@users.noreply.github.com>
2022-05-27 11:22:45 +0530

    Create filehandq3.py

---
+++ filehandq3.py
@@ -0,0 +1,10 @@
+def program7():
+    cnt = 0
+    n = int(input("Enter no. characters to read:"))
+    with open("merge.txt","r") as f1:
+       line1=f1.readline()
+       print("The first line of file:",line1)
+       nchar=f1.read(n)
+       print("First n no. of characters:", nchar)
+       nline=f1.readlines()
+       print("Line n:",nline[n])
