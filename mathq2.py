e3c84bbfb1d78347e8f090f9102527f7615e7c35
SHIVANSH666 <92948571+SHIVANSH666@users.noreply.github.com>
2022-05-27 11:31:59 +0530

    Create mathq2.py

---
+++ mathq2.py
@@ -0,0 +1,12 @@
+# Python code to demonstrate the working of
+# gcd()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = 15
+b = 5
+ 
+# returning the gcd of 15 and 5
+print ("The gcd of 5 and 15 is : ", end="")
+print (math.gcd(b, a))
