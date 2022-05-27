5363734896298d3f3b6772fca98c0f74176d8c47
SHIVANSH666 <92948571+SHIVANSH666@users.noreply.github.com>
2022-05-27 11:33:03 +0530

    Create mathq4.py

---
+++ mathq4.py
@@ -0,0 +1,11 @@
+# Python code to demonstrate the working of
+# factorial()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = 5
+ 
+# returning the factorial of 5
+print("The factorial of 5 is : ", end="")
+print(math.factorial(a))
