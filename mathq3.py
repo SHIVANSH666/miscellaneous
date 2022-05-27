e06652cd4206fe88eb18d5f650b44684a870477f
SHIVANSH666 <92948571+SHIVANSH666@users.noreply.github.com>
2022-05-27 11:32:29 +0530

    Create mathq3.py

---
+++ mathq3.py
@@ -0,0 +1,11 @@
+# Python code to demonstrate the working of
+# fabs()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = -10
+ 
+# returning the absolute value.
+print ("The absolute value of -10 is : ", end="")
+print (math.fabs(a))
